e978e0ea0c35a533c7f1e8f9e19b74c6b4ad378a
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-10-15 18:02:50 -0400

    Add Aarch64 memory extension detection code and enhance documentation for AArch64 and x86-64 ISAs: update module comments to include links to reference manuals and clarify purpose
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/cpu/isa/aarch64/mod.rs
+++ catten/src/cpu/isa/aarch64/mod.rs
@@ -1,3 +1,10 @@
+//! Aarch64 Instruction Set Architecture
+//!
+//! This module contains all aarch64-specific code. The main reference documentation
+//! for this ISA is the
+//! [ARM Architecture Reference Manual, for A-profile architecture](https://developer.arm.com/documentation/ddi0487/latest/)
+//! which we generally refer to as the "ARM ARM".
+
 pub mod init;
 pub mod interrupts;
 pub mod io;
