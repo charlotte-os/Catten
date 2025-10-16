f36134909bfddcc54388d9569c0d9a094e3aadca
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-10-16 08:45:09 -0400

    Add RISC-V64 ISA module with initial documentation

---
+++ catten/src/cpu/isa/riscv64/mod.rs
@@ -0,0 +1,7 @@
+//! The RISC-V64 Instruction Set Architecture (ISA)
+//!
+//! This module contains all riscv64-specific code. The main reference documentation
+//! for this ISA are the
+//! [RISC-V Ratified ISA Specifications](https://riscv.atlassian.net/wiki/spaces/HOME/pages/16154769/RISC-V+Technical+Specifications#ISA-Specifications)
+//! The Unprivileged Specification is referred to herein as the RVUS and the Privileged
+//! Specification is referred to as the RVPS.
