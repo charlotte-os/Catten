fa75ccca360807e9f001c19caa425b9699306259
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-10-17 14:32:33 -0400

    Bump charlottek version to 0.2.1, update hashbrown dependency, and add ARM64 SMC and Devicetree modules with documentation

---
+++ catten/src/environment/firmware/devicetree/mod.rs
@@ -0,0 +1,4 @@
+//! # Devicetree
+//!
+//! This module provides facilities for parsing flattened device trees (FDTs) and abstracts them to
+//! the common interface expected by the rest of the kernel.
--- catten/src/environment/firmware/mod.rs
+++ catten/src/environment/firmware/mod.rs
@@ -6,17 +6,25 @@
 //! power management. The firmware interface modules are optional and can be enabled or disabled
 //! using feature flags to suit the requirements of the target platform.
 //!
-//! *PC like systems arr expected to provide UEFI, ACPI, and SMBIOS firmware interfaces.
-//! *Embedded systems are expected to adhere to the Embedded Base Boot Requirements (EBBR)
-//! specification and provide a reduced subset of UEFI boot services and either ACPI tables or a
-//! Flattened Device Tree (FDT). *All ARM64 systems are expected to provide an ARM Trusted Firmware
-//! (ATF) and consequently a Secure Monitor Call (SMC) interface. *All RISC-V systems are expected
-//! to provide firmware running in M-mode that implements the Supervisor Binary Interface (SBI).
-//! *x86_64 systems do all tend to provide firmware operating in System Management Mode (SMM)
-//! however the interface to SMM interrupt calls is not standardized and thus must be accessed
-//! through the use of ACPI AML code. As such we do not provide a separate module for SMM calls.
+//! - PC like systems including servers are expected to provide the UEFI and ACPI firmware
+//!   interfaces in a manner that complies with the specifications and does not require OS specific
+//!   hacks.
+//! - Embedded systems are expected to adhere to the Embedded Base Boot Requirements (EBBR)
+//!   specification and provide a reduced subset of UEFI boot services and either ACPI tables or a
+//!   Flattened Device Tree (FDT).
+//! - All ARM64 systems are expected to provide an ARM Trusted Firmware (ATF) and consequently a
+//!   Secure Monitor Call (SMC) interface.
+//! - x86_64 systems tend to provide firmware operating in System Management Mode (SMM) however the
+//!   interface to SMM interrupt calls is not standardized and thus must be accessed through ACPI.
+//!   As such we do not provide a separate module for SMM calls.
 
 // Advanced Configuration and Power Interface (ACPI)
-pub mod acpi;
+mod acpi;
+// Device Tree
+#[cfg(not(target_arch = "x86_64"))]
+mod devicetree;
+// ARM Secure Monitor Call (SMC) Interface
+#[cfg(target_arch = "aarch64")]
+mod arm_smc;
 // Unified Extensible Firmware Interface (UEFI) Runtime Services
-pub mod uefi_rt;
+mod uefi_rt;
