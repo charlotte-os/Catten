db6764ab3ecf486e10a4aac4a11c5286872c2335
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-10-20 04:09:28 -0400

    Start implementing the basic hooks for the `acpi` crate which will be used to implement ACPI support.
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/environment/acpi/mod.rs
+++ catten/src/environment/acpi/mod.rs
@@ -1,1 +1,8 @@
+use acpi::AcpiTables;
+use spin::RwLock;
 
+//pub static ACPI_TABLES: RwLock<AcpiTables<AcpiHandler>> =
+// RwLock::new(AcpiTables::new(AcpiHandler));
+
+pub struct AcpiHandler;
+//impl acpi::AcpiHandler for AcpiHandler {}
