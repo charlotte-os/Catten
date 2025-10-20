5ea299a384694cad543e6fe989ef7fb131f81808
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-10-20 18:13:47 -0400

    Start implementing the kernel interface needed by uACPI to provide robust support for ACPI
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/environment/acpi/mod.rs
+++ catten/src/environment/acpi/mod.rs
@@ -1,8 +1,1 @@
-//use acpi::AcpiTables;
-use spin::RwLock;
-
-//pub static ACPI_TABLES: RwLock<AcpiTables<AcpiHandler>> =
-// RwLock::new(AcpiTables::new(AcpiHandler));
-
-pub struct AcpiHandler;
-//impl acpi::AcpiHandler for AcpiHandler {}
+pub mod uacpi_kernel;
