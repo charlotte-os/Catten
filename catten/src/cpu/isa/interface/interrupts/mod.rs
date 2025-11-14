777b00026eab177f0610df5da26668d3df531739
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-11-14 00:37:01 -0500

    [KNOWN GOOD] #10: Implement support for unicast IPIs on the x86-64 ISA using the x2APIC
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/cpu/isa/interface/interrupts/mod.rs
+++ catten/src/cpu/isa/interface/interrupts/mod.rs
@@ -1,17 +1,18 @@
 use crate::cpu::isa::lp::LpId;
 pub trait InterruptManagerIfce {
     type Error;
-    type Ipi;
     type IsrDesc;
+    type LocalIntCtlr: LocalIntCtlr;
     /// Initialize interrupt structures (IDT, IVT, etc.)
     fn init_interrupt_structures() -> Result<(), Self::Error>;
-    /// Initialize local interrupt controller (e.g., LAPIC, GIC, CLINT, IMSIC, etc.)
-    fn init_local_interrupt_controller() -> Result<(), Self::Error>;
-    /// Initialize system interrupt controller (e.g., IOAPIC, GIC Distributor, APLIC, etc.)
-    fn init_system_interrupt_controller() -> Result<(), Self::Error>;
-    /// Send the specified inter-processor interrupt (IPI) to the specified logical processors
-    fn send_ipi(lp_list: &[LpId], ipi: Self::Ipi);
     /// Register an interrupt handler using an ISA specific descriptor for where to install it and
     /// with what attributes
     fn register_interrupt_handler(isrd: &Self::IsrDesc) -> Result<(), Self::Error>;
+}
+
+/// # Local Interrupt Controller Interface
+pub trait LocalIntCtlr {
+    type Error;
+    /// Send an inter-processor interrupt to the specified logical processor
+    fn send_unicast_ipi(target_lp: LpId) -> Result<(), Self::Error>;
 }
--- catten/src/cpu/isa/x86_64/constants/mod.rs
+++ catten/src/cpu/isa/x86_64/constants/mod.rs
@@ -1,1 +1,2 @@
+pub mod interrupt_vectors;
 pub mod msrs;
