0ed2df008b7b2d68e95737277cee0ca350d69818
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-12-21 00:11:14 -0500

    Prep for context switching.
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/cpu/isa/x86_64/interrupts/fixed/mod.rs
+++ catten/src/cpu/isa/x86_64/interrupts/fixed/mod.rs
@@ -1,13 +1,13 @@
 //! ISRs with fixed vector numbers across all logical processors
 
+pub mod context_switch;
 pub mod exceptions;
 pub mod ipis;
 pub mod spurious;
-pub mod timer;
 
 use crate::cpu::isa::constants::interrupt_vectors::{
+    CONTEXT_SWITCH_VECTOR,
     SPURIOUS_INTERRUPT_VECTOR,
-    TIMER_INTERRUPT_VECTOR,
 };
 use crate::cpu::isa::init::gdt::KERNEL_CODE_SELECTOR;
 use crate::cpu::isa::interrupts::idt::Idt;
@@ -21,5 +21,11 @@ pub fn register_fixed_isr_gates(idt: &mut Idt) {
         false,
         true,
     );
-    idt.set_gate(TIMER_INTERRUPT_VECTOR, timer::isr_lp_timer, KERNEL_CODE_SELECTOR, false, true);
+    idt.set_gate(
+        CONTEXT_SWITCH_VECTOR,
+        context_switch::isr_context_switch,
+        KERNEL_CODE_SELECTOR,
+        false,
+        true,
+    );
 }
--- catten/src/cpu/isa/x86_64/lp/mod.rs
+++ catten/src/cpu/isa/x86_64/lp/mod.rs
@@ -1,5 +1,4 @@
 // x86_64 Logical Processor Operations
-pub mod lp_isa_data;
 pub mod ops;
 pub mod thread_context;
 
