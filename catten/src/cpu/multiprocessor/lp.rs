2d5d3a25e00d462dc4b00392549c2a44df1d2db2
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-12-22 22:47:10 -0500

    Refactor scheduler integration: update yield_lp signature and adjust bootstrapping logs to yield to the scheduler
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/cpu/multiprocessor/lp.rs
+++ catten/src/cpu/multiprocessor/lp.rs
@@ -1,8 +1,11 @@
+use alloc::collections::vec_deque::VecDeque;
+
 use crate::cpu::scheduler::threads::ThreadId;
 use crate::memory::AddressSpaceId;
 
 const AS_AFFINITY_COUNT: usize = 4095;
 
 pub struct LogicalProcessor {
-    as_affinities: [Option<AddressSpaceId>; AS_AFFINITY_COUNT],
+    pub as_affinities:  [Option<AddressSpaceId>; AS_AFFINITY_COUNT],
+    pub exec_queue_ptr: *mut VecDeque<ThreadId>,
 }
