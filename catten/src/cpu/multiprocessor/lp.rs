9d04744de0200b019f768b20025948c88bb2344a
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-12-24 18:25:18 -0500

    Refactor LogicalProcessor structure: replace raw pointer with Arc<Mutex> for exec_queue_ptr and add LpId field. Also create a setup member function to install the LP struct for each LP in an ISA abstracted manner.
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- catten/src/cpu/multiprocessor/lp.rs
+++ catten/src/cpu/multiprocessor/lp.rs
@@ -1,11 +1,37 @@
+use alloc::boxed::Box;
 use alloc::collections::vec_deque::VecDeque;
+use alloc::format;
+use alloc::sync::Arc;
 
+use spin::mutex::Mutex;
+
+use crate::cpu::isa::interface::memory::address::VirtualAddress;
+use crate::cpu::isa::lp::LpId;
+use crate::cpu::isa::lp::ops::set_lp_local_base;
 use crate::cpu::scheduler::threads::ThreadId;
-use crate::memory::AddressSpaceId;
+use crate::memory::{AddressSpaceId, VAddr};
 
 const AS_AFFINITY_COUNT: usize = 4095;
 
 pub struct LogicalProcessor {
-    pub as_affinities:  [Option<AddressSpaceId>; AS_AFFINITY_COUNT],
-    pub exec_queue_ptr: *mut VecDeque<ThreadId>,
+    pub id: LpId,
+    pub as_affinities: [Option<AddressSpaceId>; AS_AFFINITY_COUNT],
+    pub exec_queue_ptr: Arc<Mutex<VecDeque<ThreadId>>>,
+}
+
+impl LogicalProcessor {
+    pub fn setup(id: LpId, exec_queue_ptr: Arc<Mutex<VecDeque<ThreadId>>>) {
+        let lp_struct = Box::try_new(LogicalProcessor {
+            id,
+            as_affinities: [None; AS_AFFINITY_COUNT],
+            exec_queue_ptr,
+        })
+        .expect(&format!(
+            "Failed to allocate an LP struct for LP{}. Main memory is insufficient for core \
+             kernel functionality.",
+            id
+        ));
+        let vaddr = VAddr::from_mut(Box::into_raw(lp_struct));
+        set_lp_local_base(vaddr);
+    }
 }
